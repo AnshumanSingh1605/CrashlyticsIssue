a846f779759b864734d21f52bce139ae8f132256
Anshuman <anshuman1605@gmail.com>
2019-06-21 17:34:37 +0530

    Core data managed object files

---
+++ CrashlyticsIssue/Issues+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Issues+CoreDataClass.swift
+//  
+//
+//  Created by Anshuman Singh on 6/21/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Issues)
+public class Issues: NSManagedObject {
+
+}
